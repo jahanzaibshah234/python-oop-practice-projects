22efa76a74dbc645a53551b9330cb28841af4de3
Jahanzaib Khalid <154453607+jahanzaibshah234@users.noreply.github.com>
2025-09-11 22:57:18 +0500

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Static Method In Python/static-method-in-python.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+# Implement Addition class here
+class Addition:
+    
+    @staticmethod
+    def add(a, b) -> int:
+        return a + b
+        
+res = Addition.add(3, 4)
